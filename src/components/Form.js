bc1d124ade8632994c22bd768796b98d9ceff0e9
Victor <victorac2324@gmail.com>
2024-04-09 20:25:59 -0400

    used abstartion on forms to condense code (need to install npm again due to having to repo a second time)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,70 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
 
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+function Form (){
+  const [formData, setFormData] = useState({
+    firstName:"john",
+    lastName : "Henry",
+    middleName: "jack",
+    maidenName: 'Duper',
+    admin: true,
+   });
+
+  function handleChange(event){
+    const name =event.target.name
+    let value = event.target.value;
+
+  if (event.target.type === "checkbox") {
+    value = event.target.checked;
+    
+  }
+  setFormData({
+    ...formData,
+    [name]: value,
+  })
+    
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    console.log(formData)
+  }
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+    setFormData({
+      ...formData,
+      firstName:event.target.value,
+    })
   }
 
   function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+    setFormData({
+      ...formData,
+      lastName: event.target.value,
+    })
 
+  }
+  function handleMiddleNameChange (event){
+    setFormData({
+      ...formData,
+      middleName:event.target.value
+    })
+  }
+function handleMaidenNameChange (event){
+  setFormData ({
+    ...formData,
+    maidenName: event.target.value
+  })
+}
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleFirstNameChange}  value={formData.firstName} />
+      <input type="text" onChange={handleLastNameChange}  value ={formData.lastName}/>
+      <input type="text" onChange={handleMiddleNameChange} value={formData.middleName}/>
+      <input type="text" onChange={handleMaidenNameChange} value ={formData.maidenName}/>
+      <input type="checkbox" name="admin" onChange={handleChange} checked={formData.admin}/>
       <button type="submit">Submit</button>
     </form>
   );
